ae5786f6a402a145fc9ace4a0f2e5d7594ee7a6a
MuhammadZeeshanGhafoor <mirzazeeshan3738533@gmail.com>
2023-12-06 15:39:40 +0500

    mesage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 function App() {
   const [data, setData] = useState(null);
   const sendRequest = async () => {
-    const response = axios.get("http://51.20.130.9/test");
+    const response = await axios.get("http://51.20.130.9/test");
     if(response){
       console.log(response);
       setData(response.data);
@@ -17,7 +17,7 @@ function App() {
       <h1>Testing aws MERN connectivity</h1>
       <button onClick={()=> sendRequest()}>send</button>
       {data && data.response.map((ele)=>{
-        return <p>{ele.name}</p>
+        return( <p>{ele.name}</p>)
       })}
     </div>
   );
